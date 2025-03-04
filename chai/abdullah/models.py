c60deaf92efb4d17b9a1b2f713d33988344c4274
Muhammad <muhammadabdullahgohar572@gmail>
2025-03-04 06:49:52 +0500

    first commit

--- chai/abdullah/models.py
+++ chai/abdullah/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+from django.utils import timezone
 # Create your models here.
+class chaiVartity(models.Model):
+    name=models.CharField(max_length=100)
+    imge=models.ImageField(upload_to='chai')
+date_added=models.DateTimeField(default=timezone.now)
